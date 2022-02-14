53cf963679f1537439787c3f1c77bdaa8f1afa62
Maksimchik36 <Peltek1985@gmail.com>
2022-02-14 19:52:11 +0200

    autocheck of module 2 completed

--- js/autochecks.js
+++ js/autochecks.js
@@ -273,26 +273,283 @@
 // Модуль 2. Задача 21
 // Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
 
-function findLongestWord(string) {
-  // Change code below this line
-  const array = string.split("");
-  let count = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] !== " ") {
-      count += 1;
-      
+// function findLongestWord(string) {
+//   // Change code below this line
+//   let array = string.split(" ");
+//   let word = array[0];
 
-    } else {
-      console.log(count);
-      count = 0;
-    }
+//   for (i = 1; i < array.length; i += 1){
+//     if (array[i].length < word.length){
+//       continue;
+//     } else word = array[i];
+//   }
+
+// return word;
+//   // Change code above this line
+// }
+
+// findLongestWord("The quick brown fox jumped over the lazy dog") //возвращает jumped
+// findLongestWord("Google do a roll") //возвращает Google
+// findLongestWord("May the force be with you") //возвращает force
+// //findLongestWord() со случайной строкой возвращает правильное значение
 
 
-    // Change code above this line
-  }
-}
+// Модуль 2. Задача 22
+// Дополни код функции createArrayOfNumbers(min, max) так, чтобы она возвращала массив всех целых чисел от значения min до max.
 
-findLongestWord("The quick brown fox jumped over the lazy dog") //возвращает jumped
-findLongestWord("Google do a roll") //возвращает Google
-findLongestWord("May the force be with you") //возвращает force
-//findLongestWord() со случайной строкой возвращает правильное значение
+// function createArrayOfNumbers(min, max) {
+//   let numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i +=1){
+//     numbers.push(i);
+//   }
+
+//   // Change code above this line
+//   return numbers;
+// }
+
+
+// createArrayOfNumbers(1, 3) // возвращает [1, 2, 3]
+// createArrayOfNumbers(14, 17) // возвращает [14, 15, 16, 17]
+// createArrayOfNumbers(29, 34) // возвращает [29, 30, 31, 32, 33, 34]
+// В цикле for использовался метод push
+
+
+// Модуль 2. Задача 23
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел (параметр numbers) и возвращает новый массив, в котором будут только те элементы массива numbers, которые больше чем значение параметра value (число).
+
+// function filterArray(numbers, value) {
+  
+//    // Change code below this line
+// let newArray = [];
+//   for(const number of numbers){
+//     if (number <= value){
+//       continue;
+//     } else newArray.push(number);
+//   }
+
+//   console.log(newArray);
+//   // return newArray;
+//   // Change code above this line
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3) // возвращает [4, 5]
+// filterArray([1, 2, 3, 4, 5], 4) // возвращает [5]
+// filterArray([1, 2, 3, 4, 5], 5) // возвращает []
+// filterArray([12, 24, 8, 41, 76], 38) // возвращает [41, 76]
+// filterArray([12, 24, 8, 41, 76], 20) // возвращает [24, 41, 76]
+// // В цикле for использовался метод push
+
+
+// Модуль 2. Задача 24
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+//   let isContained = Boolean;
+//   return isContained = fruits.includes(fruit);
+// }
+
+// // Дополни код функции так, что если:
+// // фрукт есть в массиве, то функция возвращает true;
+// // фрукта нет в массиве, то функция возвращает false.
+// checkFruit("plum") // возвращает true
+// checkFruit("mandarin") // возвращает false
+// checkFruit("pear") // возвращает true
+// checkFruit("Pear") // возвращает false
+// checkFruit("apple") // возвращает true
+// // В функции использовался метод includes
+
+
+// Модуль 2. Задача 25
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+
+// Например, в двух массивах [1, 3, 5] и [0, 8, 5, 3] общими будут числа 3 и 5, т.к. они присутствуют в обоих исходных массивах. А числа 0, 1 и 8 присутствуют только в одном из массивов.
+
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2, и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   let newArray = [];
+//   for(let i = 0; i < array1.length; i += 1){
+//     if (array2.includes(array1[i])){
+//       newArray.push(array1[i]);
+//     }
+//   }
+//   console.log(newArray);
+// // return newArray;
+//  // Change code above this line
+// }
+
+// getCommonElements([1, 2, 3], [2, 4]) // возвращает [2]
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]) // возвращает [1, 2]
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) // возвращает [12, 27, 3]
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) // возвращает [10, 30, 40]
+// getCommonElements([1, 2, 3], [10, 20, 30]) // возвращает []
+// // В цикле for использовались методы includes и push
+
+
+// Модуль 2. Задача 26
+// Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (let element of order) {
+//     total += element;
+//   }
+
+//   // Change code above this line
+//   // return total;
+//   console.log(total);
+// }
+
+// calculateTotalPrice([12, 85, 37, 4]) // возвращает 138
+// calculateTotalPrice([164, 48, 291]) // возвращает 503
+// calculateTotalPrice([412, 371, 94, 63, 176]) // возвращает 1116
+// calculateTotalPrice([]) // возвращает 0
+
+
+// Модуль 2. Задача 27
+// Выполни рефакторинг функции filterArray(numbers, value) заменив цикл for на for...of.
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (let number of numbers) {
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   // return filteredNumbers;
+//   console.log(filteredNumbers);
+//   // Change code above this line
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3) // возвращает [4, 5]
+// filterArray([1, 2, 3, 4, 5], 4) // возвращает [5]
+// filterArray([1, 2, 3, 4, 5], 5) // возвращает []
+// filterArray([12, 24, 8, 41, 76], 38) // возвращает [41, 76]
+// filterArray([12, 24, 8, 41, 76], 20) // возвращает [24, 41, 76]
+// // Функция calculateTotalPrice() использует цикл for..of
+
+
+// Модуль 2. Задача 28
+// Дополни выражения остатка от деления так, чтобы код проходил тесты.
+
+// Change code below this line
+
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3;
+
+// Значение переменной a это число 0
+// Значение переменной b это число 1
+// Значение переменной c это число 3
+// Значение переменной d это число 5
+// Значение переменной e это число 2
+
+// Модуль 2. Задача 29
+// Напиши функцию getEvenNumbers(start, end) которая возвращает массив всех чётных чисел от start до end. Чётным считается число которое делится на 2 без остатка (10 % 2 === 0).
+
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+//    let newArray = [];
+//    for(let i = start; i <= end; i += 1){
+//      if (i % 2 !== 0){
+//        continue;
+//      } else newArray.push(i);
+//    }
+// // return newArray;
+//   console.log(newArray);
+//     // Change code above this line
+//   }
+
+// getEvenNumbers(2, 5) // возвращает [2, 4]
+// getEvenNumbers(3, 11) // возвращает [4, 6, 8, 10]
+// getEvenNumbers(6, 12) // возвращает [6, 8, 10, 12]
+// getEvenNumbers(8, 8) // возвращает [8]
+// getEvenNumbers(7, 7) // возвращает []
+
+
+// Модуль 2. Задача 30
+// Дополни код так, чтобы в переменную number записывалось первое число от start до end, которое делится на 5 без остатка.
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+// console.log(number);
+
+
+// Объявлена переменная start со значением 6
+// Объявлена переменная end со значением 27
+// Объявлена переменная number без инициализации
+// Итоговое значение переменной number равно 10
+// В цикле for используется break для выхода до завершения всех итераций цикла
+
+
+// Модуль 2. Задача 31
+// Выполни рефакторинг функции findNumber(start, end, divisor) так, чтобы она:
+// возвращала первое число от start до end, которое делится на divisor без остатка
+// не использовала оператор break
+// не использовала переменную number
+
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % divisor === 0) {
+    
+//   return i;
+//     }
+//   }
+
+//   // Change code above this line
+// }
+
+// findNumber(2, 6, 5) // возвращает 5
+// findNumber(8, 17, 3) // возвращает 9
+// findNumber(6, 9, 4) // возвращает 8
+// findNumber(16, 35, 7) // возвращает 21
+// // В цикле for не должен использоваться break для выхода до завершения всех итераций цикла
+
+
+// Модуль 2. Задача 32
+// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+// function includes(array, value) {
+//   // Change code below this line
+//   let isContained = false;
+// for (let element of array){
+//   if (element === value) {
+//     isContained = true;
+//   } 
+// }
+// // return isContained;
+//   console.log(isContained);
+//   // Change code above this line
+// }
+
+// includes([1, 2, 3, 4, 5], 3) // возвращает true
+// includes([1, 2, 3, 4, 5], 17) // возвращает false
+// includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") // возвращает true
+// includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") // возвращает false
+// includes(["apple", "plum", "pear", "orange"], "plum") // возвращает true
+// includes(["apple", "plum", "pear", "orange"], "kiwi") // возвращает false
+// // Вызов includes() для случайного массива со случайным value возвращает верный boolean
+// // В функции includes используется for, return, но не метод массива includes
