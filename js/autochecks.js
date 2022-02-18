41546bdb1554f8e0e1b1bf5cc6b0c9827b15b585
Maksimchik36 <Peltek1985@gmail.com>
2022-02-18 22:13:38 +0200

    module-3 task-20

--- js/autochecks.js
+++ js/autochecks.js
@@ -1205,3 +1205,37 @@
 
 
 // Модуль 3. Задача 20
+
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let totalPrice = 0;
+//   for(const product of products){
+//     if (product.name === productName){
+//       totalPrice = product.price * product.quantity
+//     }
+//   }
+//   return totalPrice;
+
+//   // Пиши код выше этой строки
+// }
+
+
+// // Объявлена функция calculateTotalPrice(productName)
+// console.log(calculateTotalPrice("Blaster"));  // возвращает 0
+// console.log(calculateTotalPrice("Radar"))  // возвращает 5200
+// console.log(calculateTotalPrice("Droid"))  // возвращает 2800
+// console.log(calculateTotalPrice("Grip"))  // возвращает 10800
+// console.log(calculateTotalPrice("Scanner"))  // возвращает 8100
+
+
+// Модуль 3. Задача 21
+
