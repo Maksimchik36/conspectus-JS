9a0fb7dcb34ac3997bc4a51688a1dec8819b40c7
Maksimchik36 <Peltek1985@gmail.com>
2023-04-10 23:48:24 +0300

    freecodecamp tasks
    finish bazic and start ES6

--- js/freecodecamp-bazic-js.js
+++ js/freecodecamp-bazic-js.js
@@ -479,3 +479,105 @@
 
 
 
+
+
+
+// 112. Use Recursion to Create a Countdown
+// In a previous challenge, you learned how to use recursion to replace a for loop. Now, let's look at a more complex function that returns an array of consecutive integers starting with 1 through the number passed to the function.
+
+// As mentioned in the previous challenge, there will be a base case. The base case tells the recursive function when it no longer needs to call itself. It is a simple case where the return value is already known. There will also be a recursive call which executes the original function with different arguments. If the function is written correctly, eventually the base case will be reached.
+
+// For example, say you want to write a recursive function that returns an array containing the numbers 1 through n. This function will need to accept an argument, n, representing the final number. Then it will need to call itself with progressively smaller values of n until it reaches 1. You could write the function as follows:
+
+// function countup(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const countArray = countup(n - 1);
+//     countArray.push(n);
+//     return countArray;
+//   }
+// }
+// console.log(countup(5));
+// The value [1, 2, 3, 4, 5] will be displayed in the console.
+
+// At first, this seems counterintuitive since the value of n decreases, but the values in the final array are increasing. This happens because the push happens last, after the recursive call has returned. At the point where n is pushed into the array, countup(n - 1) has already been evaluated and returned [1, 2, ..., n - 1].
+
+// We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter. If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1]. Your function must use recursion by calling itself and must not use loops of any kind.
+
+
+// // Only change code below this line
+// function countdown(n) {
+//     let countArray = []
+//     if (n < 1) {
+//         console.log("n < 1", countArray);
+//         return countArray;
+//     }
+//         else {
+//         countArray = countdown(n - 1);
+//         countArray.unshift(n);
+//         console.log("countArray", countArray);
+//         return countArray;
+//     }
+// }
+// Only change code above this line
+
+
+// countdown(-1) // should return an empty array.
+// countdown(10) // should return [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// countdown(5) // should return [5, 4, 3, 2, 1]
+
+// Your code should not rely on any kind of loops (for, while or higher order functions such as forEach, map, filter, and reduce).
+// You should use recursion to solve this problem.
+// Global variables should not be used to cache the array.
+
+
+
+
+// 113. Use Recursion to Create a Range of Numbers
+// Continuing from the previous challenge, we provide you another opportunity to create a recursive function to solve a problem.
+
+// We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+
+
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum > endNum) {
+        console.log("result []");
+        return [];
+    }
+    else {
+       const newArray = rangeOfNumbers(startNum, endNum - 1);
+        newArray.push(endNum);
+        console.log("newArray", newArray);
+        return newArray;
+    }
+};
+
+
+// Алгоритм выполнения рекурсии для rangeOfNumbers(startNum, endNum) на примере rangeOfNumbers(1, 5):
+// rangeOfNumbers(1, 5) - становится в стэк
+// rangeOfNumbers(startNum, endNum - 1)
+// rangeOfNumbers(1, 4) - становится в стэк
+// rangeOfNumbers(startNum, endNum - 1)
+// rangeOfNumbers(1, 3) - становится в стэк
+// rangeOfNumbers(startNum, endNum - 1)
+// rangeOfNumbers(1, 2) - становится в стэк
+// rangeOfNumbers(startNum, endNum - 1)
+// rangeOfNumbers(1, 1) - становится в стэк
+// rangeOfNumbers(startNum, endNum - 1)
+// rangeOfNumbers(1, 0) - из if возвращает пустой массив, и возвращается в стэк за ф-ей со стр 567:
+// rangeOfNumbers(1, 1) - пушит 1 в массив [] и возвращается в стэк за ф-ей со стр 565
+// rangeOfNumbers(1, 2) - пушит 2 в массив [1] и возвращается в стэк за ф-ей со стр 563
+// rangeOfNumbers(1, 3) - пушит 3 в массив [1, 2] и возвращается в стэк за ф-ей со стр 561
+// rangeOfNumbers(1, 4) - пушит 4 в массив [1, 2, 3] и возвращается в стэк за ф-ей со стр 559
+// rangeOfNumbers(1, 5) - пушит 5 в массив [1, 2, 3, 4] . в стэке больше нет ф-ий, операция завершается 
+
+
+// Your function should return an array.
+//Your code should not use any loop syntax (for or while or higher order functions such as forEach, map, filter, or reduce).
+// rangeOfNumbers should use recursion (call itself) to solve this challenge.
+// rangeOfNumbers(5, 1) // should return [].
+// rangeOfNumbers(1, 5) // should return [1, 2, 3, 4, 5].
+// rangeOfNumbers(6, 9) // should return [6, 7, 8, 9].
+// rangeOfNumbers(4, 4) // should return [4].
+// Global variables should not be used to cache the array.
