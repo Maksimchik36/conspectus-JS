c3b5ca793750501f4ab14e9aedc314868554441f
Maksimchik36 <Peltek1985@gmail.com>
2023-04-16 23:21:07 +0300

    freecodecamp
    regular expressions

--- js/freecodecamp-regular-expressions.js
+++ js/freecodecamp-regular-expressions.js
@@ -153,11 +153,245 @@
 // solution
 // let extractStr = "Extract the word 'coding' from this string.";
 // let codingRegex = /coding/;
-// let result = extractStr.match(codingRegex);  
+// let result = extractStr.match(codingRegex);
 // console.log("result", result);
-// "где ищем".match("что ищем") - match() - синтаксис противоположен методу test() 
+// "где ищем".match("что ищем") - match() - синтаксис противоположен методу test()
 // "что ищем".test("где ищем")  - test()
     
 // The result should have the string coding
 // Your regex codingRegex should search for the string coding
 // You should use the .match() method.
+
+
+
+
+// 6. Find More Than the First Match
+// So far, you have only been able to extract or search a pattern once.
+
+// let testStr = "Repeat, Repeat, Repeat";
+// let ourRegex = /Repeat/;
+// testStr.match(ourRegex);
+// Here match would return ["Repeat"].
+
+// To search or extract a pattern more than once, you can use the global search flag: g.
+
+// let repeatRegex = /Repeat/g;
+// testStr.match(repeatRegex);
+// And here match returns the value ["Repeat", "Repeat", "Repeat"]
+
+// Using the regex starRegex, find and extract both Twinkle words from the string twinkleStar.
+// You can have multiple flags on your regex like /search/gi
+
+// task
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /change/; // Change this line
+// let result = twinkleStar; // Change this line
+
+// solution
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /Twinkle/gi;
+// let result = twinkleStar.match(starRegex);
+
+// Your regex starRegex should use the global flag g
+// Your regex starRegex should use the case insensitive flag i
+// Your match should match both occurrences of the word Twinkle
+// Your match result should have two elements in it.
+
+
+
+
+// 7. Match Anything with Wildcard Period
+// Sometimes you won't (or don't need to) know the exact characters in your patterns. Thinking of all words that match, say, a misspelling would take a long time. Luckily, you can save time using the wildcard character: .
+// The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+
+// let humStr = "I'll hum a song";
+// let hugStr = "Bear hug";
+// let huRegex = /hu./;
+// huRegex.test(humStr);
+// huRegex.test(hugStr);
+// Both of these test calls would return true.
+
+// Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
+
+// task
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /change/; // Change this line
+// let result = unRegex.test(exampleStr);
+
+//solution
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un/;
+// let result = unRegex.test(exampleStr);
+
+// You should use the .test() method.
+// You should use the wildcard character in your regex unRegex
+// Your regex unRegex should match run in the string Let us go on a run.
+// Your regex unRegex should match sun in the string The sun is out today.
+// Your regex unRegex should match fun in the string Coding is a lot of fun.
+// Your regex unRegex should match pun in the string Seven days without a pun makes one weak.
+// Your regex unRegex should match nun in the string One takes a vow to be a nun.
+// Your regex unRegex should match bun in the string She got fired from the hot dog stand for putting her hair in a bun.
+// Your regex unRegex should not match the string There is a bug in my code.
+// Your regex unRegex should not match the string Catch me if you can.
+
+
+
+
+// 8. Match Single Character with Multiple Possibilities
+// You learned how to match literal patterns (/literal/) and wildcard character (/./). Those are the extremes of regular expressions, where one finds exact matches and the other matches everything. There are options that are a balance between the two extremes.
+// You can search for a literal pattern with some flexibility with character classes. Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+// For example, you want to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u.
+
+// let bigStr = "big";
+// let bagStr = "bag";
+// let bugStr = "bug";
+// let bogStr = "bog";
+// let bgRegex = /b[aiu]g/;
+// bigStr.match(bgRegex);
+// bagStr.match(bgRegex);
+// bugStr.match(bgRegex);
+// bogStr.match(bgRegex);
+// In order, the four match calls would return the values ["big"], ["bag"], ["bug"], and null.
+
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+//  Be sure to match both upper- and lowercase vowels.
+
+// task
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /change/; // Change this line
+// let result = vowelRegex; // Change this line
+
+// solution
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi;
+// let result = quoteSample.match(vowelRegex);
+
+// Waiting:You should find all 25 vowels.
+// Waiting:Your regex vowelRegex should use a character class.
+// Waiting:Your regex vowelRegex should use the global flag.
+// Waiting:Your regex vowelRegex should use the case insensitive flag.
+// Waiting:Your regex should not match any consonants.
+
+
+
+
+// 9. Match Letters of the Alphabet
+// You saw how you can use character sets to specify a group of characters to match, but that's a lot of typing when you need to match a large range of characters (for example, every letter in the alphabet). Fortunately, there is a built-in feature that makes this short and simple.
+
+// Inside a character set, you can define a range of characters to match using a hyphen character: -.
+
+// For example, to match lowercase letters a through e you would use [a-e].
+
+// let catStr = "cat";
+// let batStr = "bat";
+// let matStr = "mat";
+// let bgRegex = /[a-e]at/;
+// catStr.match(bgRegex);
+// batStr.match(bgRegex);
+// matStr.match(bgRegex);
+// In order, the three match calls would return the values ["cat"], ["bat"], and null.
+
+// Match all the letters in the string quoteSample.
+// Be sure to match both uppercase and lowercase letters.
+
+// task
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /change/; // Change this line
+// let result = alphabetRegex; // Change this line
+
+//solution
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi;
+// let result = quoteSample.match(alphabetRegex) ;
+
+// Your regex alphabetRegex should match 35 items.
+// Your regex alphabetRegex should use the global flag.
+// Your regex alphabetRegex should use the case insensitive flag.
+
+
+
+
+
+// 10. Match Numbers and Letters of the Alphabet
+// Using the hyphen (-) to match a range of characters is not limited to letters. It also works to match a range of numbers.
+
+// For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+
+// Also, it is possible to combine a range of letters and numbers in a single character set.
+
+// let jennyStr = "Jenny8675309";
+// let myRegex = /[a-z0-9]/ig;
+// jennyStr.match(myRegex);
+
+// Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+
+// task
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /change/; // Change this line
+// let result = myRegex; // Change this line
+
+// solution
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/gi;
+// let result = quoteSample.match(myRegex);
+
+// Your regex myRegex should match 17 items.
+// Your regex myRegex should use the global flag.
+// Your regex myRegex should use the case insensitive flag.
+
+
+
+
+// 11. Match Single Characters Not Specified
+// So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+
+// To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+
+// For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+
+// Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+
+// task
+// let quoteSample = "3 blind mice.";
+// let myRegex = /change/; // Change this line
+// let result = myRegex; // Change this line
+
+//solution
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^0-9aeiou]/gi;
+// let result = quoteSample.match(myRegex);
+
+// Your regex myRegex should match 9 items.
+// Your regex myRegex should use the global flag.
+// Your regex myRegex should use the case insensitive flag.
+
+
+
+
+
+// 12. Match Characters that Occur One or More Times
+// Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
+// You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+// For example, /a+/g would find one match in abc and return ["a"]. Because of the +, it would also find a single match in aabc and return ["aa"].
+// If it were instead checking the string abab, it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no a in the string bcd, it wouldn't find a match.
+
+// You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses the + sign.
+
+// task
+// let difficultSpelling = "Mississippi";
+// let myRegex = /change/; // Change this line
+// let result = difficultSpelling.match(myRegex);
+
+// solution
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/g;
+// let result = difficultSpelling.match(myRegex); // выдает массив значений 1 буквы, общее кол-во которых в слове больше 1
+
+
+
+
+
+
+// Your regex myRegex should use the + sign to match one or more s characters.
+// Your regex myRegex should match 2 items.
+// The result variable should be an array with two matches of ss
